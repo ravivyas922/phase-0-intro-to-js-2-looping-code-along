2c2a8e0cb1dfc3cf7b0ee44d2148fed98c16e10c
Ravi <ravivyas922@gmail.com>
2024-04-09 10:49:40 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arr, event)
+{
+    let newArray = [...arr];
+    for (let i = 0; i <arr.length; i++)
+    {
+        newArray[i] = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(int)
+{
+    while(int >=0)
+    {
+        console.log(int);
+        int--;
+    }
+}
